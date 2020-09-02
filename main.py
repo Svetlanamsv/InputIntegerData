ffd0232e23e3ba6468217303c5ea4e2366f14401
Svetlanamsv <svetikmsv34@gmail.com>
2020-09-02 20:37:08 +0300

    commit message

---
+++ main.py
@@ -0,0 +1,19 @@
+
+x = input("Enter the first number:")
+print("The first number is " + x)
+y = input("Enter the second number:")
+print("The second number is " + y)
+x = int(x)
+y = int(y)
+a = x + y
+b = x - y
+c = x * y
+d = x / y
+a = int(a)
+b = int(b)
+c = int(c)
+d = float(d)
+print("Addition ", a)
+print("Subtraction ", b)
+print("Multiplication ", c)
+print("Division ", d)
